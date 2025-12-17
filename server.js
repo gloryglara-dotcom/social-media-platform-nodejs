addf82cf3c112c1a81c70a7a2fce3a106f2f9afe
gloryglara-dotcom <gloryglara@gmail.com>
2025-12-17 11:42:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,23 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+let users = [];
 let posts = [];
 
-app.post("/post", (req, res) => {
-  posts.push(req.body);
-  res.json({ message: "Post created" });
+app.post("/register",(req,res)=>{
+ users.push(req.body);
+ res.json({message:"User registered"});
 });
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.post("/post",(req,res)=>{
+ posts.push(req.body);
+ res.json({message:"Post created"});
 });
 
-app.listen(3001, () => {
-  console.log("Social Media server running on port 3001");
+app.get("/posts",(req,res)=>{
+ res.json(posts);
 });
+
+app.listen(3001,()=>console.log("Social media app running"));
+
+ 
